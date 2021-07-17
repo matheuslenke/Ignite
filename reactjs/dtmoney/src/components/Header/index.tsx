f7b17c77df27f176e2fe74365e9ae81ac162d331
Matheus Lenke <matheus.l.coutinho@edu.ufes.br>
2021-07-17 16:33:57 -0300

    React-m2: Consumindo API done

---
+++ reactjs/dtmoney/src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import { MouseEventHandler, useState } from 'react'
+import logoImg from '../../assets/logo.svg'
+import { Container, Content } from './styles'
+
+interface HeaderProps {
+    onOpenNewTransactionModal: MouseEventHandler;
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
+
+    return (
+        <Container>
+            <Content>
+                <img src={logoImg} alt="dtmoney" />
+                <button type="button" onClick={ onOpenNewTransactionModal } >
+                    Nova transação
+                </button>
+            </Content>
+        </Container>
+    )
+}
