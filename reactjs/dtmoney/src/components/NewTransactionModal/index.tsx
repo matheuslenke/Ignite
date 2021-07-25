dab35703fb848a7f03b1757b27f41250de0843ce
Matheus Lenke <matheus.l.coutinho@edu.ufes.br>
2021-07-25 15:33:03 -0300

    Reactjs-m02: dtmoney done

--- reactjs/dtmoney/src/components/NewTransactionModal/index.tsx
+++ reactjs/dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,10 +1,12 @@
 import Modal from 'react-modal';
+import { FormEvent, useState } from 'react';
+
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import closeImg from '../../assets/close.svg';
+
 import { Container, TransactionTypeContainer, RadioBox } from './styles';
-import { FormEvent, useState } from 'react';
-import { api } from '../../services/api';
+import { useTransactions } from '../../hooks/TransactionsContext';
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -12,17 +14,28 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+    const { createTransaction } = useTransactions();
+
     const [title, setTitle] = useState('');
-    const [value, setValue] = useState(0);
+    const [amount, setAmount] = useState(0);
     const [category, setCategory] = useState('');
     const [type, setType] = useState('deposit');
 
-    function handleCreateNewTransaction(event: FormEvent) {
+    async function handleCreateNewTransaction(event: FormEvent) {
         event.preventDefault();
 
-        const data = {title, value, category, type};
+        await createTransaction({
+            title,
+            amount: amount,
+            category,
+            type,
+        })
 
-        api.post('/transactions', data).then(response => console.log(response));
+        setTitle('');
+        setAmount(0);
+        setCategory('');
+        setType('deposit');
+        onRequestClose();
     }
 
     return (
@@ -52,9 +65,9 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
                 <input 
                     type="number"
                     placeholder="Valor"
-                    value={value} 
+                    value={amount} 
                     onChange={ event => 
-                    setValue(Number(event.target.value))} 
+                    setAmount(Number(event.target.value))} 
                 />
 
                 <TransactionTypeContainer>
